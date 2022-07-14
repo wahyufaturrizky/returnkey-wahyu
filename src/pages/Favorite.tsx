d6762d6a1fb72c0cf81e34f10b7d7aef23f93389
wahyu fatur rizki <wahyu.rizky@docdoc.com>
2022-07-14 21:52:10 +0700

    usecallback provide

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -49,7 +49,7 @@ const Home = () => {
           })
         );
 
-        window.location.reload();
+        handleCheckAuth();
       } else {
         await localStorage.setItem(
           "userProfile1",
@@ -60,7 +60,7 @@ const Home = () => {
           })
         );
 
-        window.location.reload();
+        handleCheckAuth();
       }
     } else {
       if (stateUserProfile2?.favorite.includes(data)) {
@@ -77,7 +77,7 @@ const Home = () => {
           })
         );
 
-        window.location.reload();
+        handleCheckAuth();
       } else {
         await localStorage.setItem(
           "userProfile2",
@@ -88,7 +88,7 @@ const Home = () => {
           })
         );
 
-        window.location.reload();
+        handleCheckAuth();
       }
     }
   };
